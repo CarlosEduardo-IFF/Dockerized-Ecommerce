2744bfff7387614c2c4c58a29fcd0498744a7caf
CarlosEduardo-IFF <149334691+CarlosEduardo-IFF@users.noreply.github.com>
2025-04-23 16:06:05 -0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,5 @@
+ECR_REGISTRY="237487375715.dkr.ecr.us-east-1.amazonaws.com"
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REGISTRY
+docker build -t app-ecommerce .
+docker tag app-ecommerce:latest $ECR_REGISTRY/app-ecommerce:latest
+docker push $ECR_REGISTRY/app-ecommerce:latest
