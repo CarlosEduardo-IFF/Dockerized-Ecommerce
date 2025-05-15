56c35fdda41486da73dc4c3c1626fccb7113a22c
CarlosEduardo-IFF <149334691+CarlosEduardo-IFF@users.noreply.github.com>
2025-05-15 08:08:11 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 ECR_REGISTRY="237487375715.dkr.ecr.us-east-1.amazonaws.com"
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REGISTRY
-docker build -t app-ecommerce-carlos .
-docker tag app-ecommerce-carlos:latest $ECR_REGISTRY/app-ecommerce-carlos:latest
-docker push $ECR_REGISTRY/app-ecommerce-carlos:latest
+docker build -t ecommerce_repository .
+docker tag ecommerce_repository:latest $ECR_REGISTRY/ecommerce_repository:latest
+docker push $ECR_REGISTRY/ecommerce_repository:latest
